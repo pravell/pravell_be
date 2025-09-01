a6352a977df9960fbb7677a4041b60f104bf35f7
kahyun0255 <kkh9946@naver.com>
2025-09-01 17:48:13 +0900

    init: 초기 설정

---
+++ src/main/java/com/pravell/PravellApplication.java
@@ -0,0 +1,13 @@
+package com.pravell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PravellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PravellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pravell/PravellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
